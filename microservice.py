9dbc5a3c9b65337079fd61b780beb60cdddbfab4
squidstudent <squid.student@protonmail.com>
2020-05-21 23:03:51 +0000

    added multiple files and added a new microservice endpoint

--- microservice.py
+++ microservice.py
@@ -3,6 +3,8 @@ from flask import request
 from flask import jsonify
 
 import store
+import covid19
+import json
 
 app = Flask(__name__)
 
@@ -30,3 +32,21 @@ def getPosts():
     name = request.args.get('name')
     posts = store.searchTextData(name)
     return jsonify(posts)
+
+@app.route('/sendSinglePost', methods=['POST'])
+
+def sendSinglePost():
+
+    response = {}
+
+    if request.method == 'POST':
+
+        options = json.loads(request.data)
+
+        filename = covid19.getImageFilename(options)
+
+        options['url'] = store.addImageData(filename)
+
+        store.addTextData(options)
+
+    return jsonify(options)
